90f64634ff318a42fa2edab9e840a869a9e2a7f3
Jimmi08 <kika200598>
2017-09-09 16:21:44 +0200

    version 1.2

--- e_header.php
+++ e_header.php
@@ -4,17 +4,10 @@ $recaptchaSiteKey = e107::pref('recaptcha', 'sitekey');
 
 		if($recaptchaActive)
 		{
-		 // e107::js("footer", "https://www.google.com/recaptcha/api.js?onload=myCallBack" , 'jquery');
-		 
-		  /* WORKROUND  you can't use async defer with e107::js now) */
-		if(!function_exists("theme_foot")) {	
-			function theme_foot() {			
-			 // e107::js("footer", "https://www.google.com/recaptcha/api.js" , 'jquery'); 
-			 echo '<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+ 
+  e107::js("footer-inline", '<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
 		        async defer>
-		    </script>';
-		}
-    }
+		    </script>' );
 		 
 		$script = "	var onloadCallback = function() {
 		    $('.g-recaptcha').each(function(){
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Source and thanks:
+
+https://github.com/e107inc/visualcaptcha
+
+https://github.com/archer-developer/recaptcha
+
+https://webdesign.tutsplus.com/tutorials/how-to-integrate-no-captcha-recaptcha-in-your-website--cms-23024
+
+http://stackoverflow.com/questions/1241947/how-do-i-show-multiple-recaptchas-on-a-single-page
+
